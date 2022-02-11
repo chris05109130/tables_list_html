aa8df1b2f9deaafe4772883709a1ddc3afa4bd4e
chris05109130 <92364093+chris05109130@users.noreply.github.com>
2022-02-11 02:43:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tables and List</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+  <section id="mainSection">
+    <h1 class="explain">Tables</h1>
+    <h1 class="explain">Tables are a way to display data on a webpage</h1>
+
+    <p class="example">Example of a table is </p>
+    <p class="example">Table for favorite food</p>
+    <table border='1'>
+      <tr>
+        <th>Favorite Food of mine</th>
+      </tr>
+      <tr>
+
+        <td>Pizza</td>
+        <td>Chicken</td>
+        <td>Subs</td>
+        <td>Chicken Wings</td>
+        <td>Chicken Cutlet</td>
+        <td>tv dinner</td>
+      </tr>
+      <tr>
+        <td>Yes</td>
+        <td>Yes</td>
+        <td>Yes</td>
+        <td>Yes</td>
+        <td>Yes</td>
+        <td>No</td>
+
+      </tr>
+    </table>
+    <h1 class="explain">List</h1>
+    <h1 class="explain">List are ways to make a list for your webpage and used as naviation bars</h1>
+
+    <p class="example"></p>
+    <p class="example">List of Numbers</p>
+    <p class="example">Unordered (ul) and ordered list (ol) are most common</p>
+    <p class="example">Difference is unordered will give you bullet points and ol will number the list</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </ul>
+    <ol>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ol>
+
+
+
+
+
+  </section>
+
+</body>
+</html>
